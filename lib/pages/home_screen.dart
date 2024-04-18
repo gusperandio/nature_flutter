a26ca9bd2334573eb6fc6d8831c63159bebd3e06
Gustavo <gustavo.sperandio25@gmail.com>
2024-04-17 21:47:28 -0300

    Structure Home

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -31,12 +31,14 @@ class HomeScreen extends StatelessWidget {
     final _texts = getLocalizedTexts(language);
 
     return Scaffold(
-      body: Column(
+        body: Container(
+      color: const Color(0xFFE6EFF8),
+      child: Column(
         children: [
           Stack(
             children: [
               Container(
-                height: 400,
+                height: 350,
                 width: double.infinity,
                 decoration: BoxDecoration(
                   borderRadius: const BorderRadius.only(
@@ -96,7 +98,7 @@ class HomeScreen extends StatelessWidget {
                   height: 100,
                   decoration: BoxDecoration(
                       color: const Color(0xFFF2CB05),
-                      borderRadius: BorderRadius.circular(16)),
+                      borderRadius: BorderRadius.circular(12)),
                   child: Column(
                     children: [
                       const SizedBox(height: 8),
@@ -119,7 +121,7 @@ class HomeScreen extends StatelessWidget {
                   height: 100,
                   decoration: BoxDecoration(
                       color: const Color(0xFFF29F05),
-                      borderRadius: BorderRadius.circular(16)),
+                      borderRadius: BorderRadius.circular(12)),
                   child: Column(
                     children: [
                       const SizedBox(height: 8),
@@ -142,7 +144,7 @@ class HomeScreen extends StatelessWidget {
                   height: 100,
                   decoration: BoxDecoration(
                       color: const Color(0xFFF28705),
-                      borderRadius: BorderRadius.circular(16)),
+                      borderRadius: BorderRadius.circular(12)),
                   child: Column(
                     children: [
                       const SizedBox(height: 8),
@@ -163,87 +165,162 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
           ),
-          const SizedBox(height: 20),
-          Center(
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Container(
-                  width: 100,
-                  height: 200,
-                  decoration: BoxDecoration(
-                      color: const Color(0xFFF2CB05),
-                      borderRadius: BorderRadius.circular(16)),
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 8),
-                      const Icon(
-                        Icons.import_contacts,
-                        size: 40,
-                      ),
-                      Text(_texts['card1']!,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            color: Color(0xFF222222),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          )),
-                    ],
-                  ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(30.0, 12.0, 12.0, 10.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                _texts['Description']!,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
                 ),
-                Container(
-                  width: 100,
-                  height: 200,
-                  decoration: BoxDecoration(
-                      color: const Color(0xFFF29F05),
-                      borderRadius: BorderRadius.circular(16)),
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 8),
-                      const Icon(
-                        Icons.nature_people,
-                        size: 40,
-                      ),
-                      Text(_texts['card2']!,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            color: Color(0xFF222222),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          )),
-                    ],
-                  ),
-                ),
-                Container(
-                  width: 100,
-                  height: 200,
-                  decoration: BoxDecoration(
-                      color: const Color(0xFFF28705),
-                      borderRadius: BorderRadius.circular(16)),
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 8),
-                      const Icon(
-                        Icons.wallpaper,
-                        size: 40,
-                      ),
-                      Text(_texts['card3']!,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            color: Color(0xFF222222),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          )),
-                    ],
-                  ),
-                ),
+              ),
+            ),
+          ),
+          Container(
+            width: 350,
+            height: 280,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(12)),
+            child: Column(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 60,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.grey,
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.asset(
+                              'assets/dices.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                _texts['exTitle1']!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                _texts['exSubTitle1']!,
+                                style: const TextStyle(
+                                  color: Colors.black54,
+                                  fontSize: 10,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )),
+                Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 60,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.grey,
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.asset(
+                              'assets/atoz.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                _texts['exTitle2']!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                _texts['exSubTitle2']!,
+                                style: const TextStyle(
+                                  color: Colors.black54,
+                                  fontSize: 10,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )),
+                Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 60,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.grey,
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.asset(
+                              'assets/frog.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                _texts['exTitle3']!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                _texts['exSubTitle3']!,
+                                style: const TextStyle(
+                                  color: Colors.black54,
+                                  fontSize: 10,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )),
               ],
             ),
           )
         ],
       ),
-    );
+    ));
   }
 
   Map<String, String> getLocalizedTexts(bool language) {
@@ -256,6 +333,19 @@ class HomeScreen extends StatelessWidget {
       'card1': language ? "+ 3K \nITEMS" : "+ 3Mil \nITEMS",
       'card2': language ? "Over 5 \nBIOMES" : "Mais de 5\n BIOMAS",
       'card3': language ? "+ 150 \nIMAGES" : "+ 150 \nIMAGENS",
+      'Description': language ? 'Research offered' : 'Pesquisas oferecidas',
+      'exTitle1': language ? "Random" : "Aleatório",
+      'exTitle2': language ? "Dictionary" : "Dicionário",
+      'exTitle3': language ? "Especific" : "Específico",
+      'exSubTitle1': language
+          ? "Randomly search mode, where it brings an item with an image in a random way."
+          : "Pesquise no modo aleatório, onde traz um item com imagem de forma randomica.",
+      'exSubTitle2': language
+          ? "Search for items by a letter, the result will be all items with the requested letter."
+          : "Procure items por uma letra, o resultado será todos items com a letra solicitada.",
+      'exSubTitle3': language
+          ? "Search for a specific item according to its name."
+          : "Procure um item específico de acordo com o nome.",
     };
   }
 }
